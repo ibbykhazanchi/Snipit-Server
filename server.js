53c7f273cbcb0df5c161463af53b65f82f0ab90a
ibbykhazanchi <khajanchi.ibrahim@gmail.com>
2024-01-01 12:20:01 -0500

    linting

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const bodyParser = require('body-parser');
-require('dotenv').config()
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // MongoDB connection string
-const mongoURI = process.env.MONGO_URI
+const mongoURI = process.env.MONGO_URI;
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
 // Endpoint to add a user to the database
-app.post('/addUser', async (req, res) => {
+app.post("/addUser", async (req, res) => {
   let _client = null;
   try {
     const { botId, accessToken, profile } = req.body;
@@ -24,87 +24,92 @@ app.post('/addUser', async (req, res) => {
     await client.connect();
 
     const database = client.db("Snipit");
-    const collection = database.collection('users');
+    const collection = database.collection("users");
 
     const document = {
       _id: botId,
       accessToken: accessToken,
-      profile: profile
-    }
+      profile: profile,
+    };
 
     await collection.insertOne(document);
 
-    res.json({ success: true, message: 'User added to the database.' });
+    res.json({ success: true, message: "User added to the database." });
   } catch (error) {
     console.error(error);
-    if(error.code == 11000){
+    if (error.code == 11000) {
       // duplicate user error
-      res.json({success: true, message: 'User already in database'})
+      res.json({ success: true, message: "User already in database" });
     } else {
-      res.status(500).json({ success: false, message: `Internal Server Error ${error}` });
+      res
+        .status(500)
+        .json({ success: false, message: `Internal Server Error ${error}` });
     }
   } finally {
-    if(_client)
-      _client.close();
+    if (_client) _client.close();
   }
 });
 
 // Endpoint to get an account by bot_id
-app.get('/getUser/:botId', async (req, res) => {
+app.get("/getUser/:botId", async (req, res) => {
   const botId = req.params.botId;
-  if(!botId){
-    return res.status(400).json({ success: false, message: 'Incorrect botId provided' });
+  if (!botId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Incorrect botId provided" });
   }
 
   let _client = null;
   try {
     const client = new MongoClient(mongoURI);
-    _client = client
+    _client = client;
     await client.connect();
 
     const database = client.db("Snipit");
-    const collection = database.collection('users');
+    const collection = database.collection("users");
 
-    const account = await collection.findOne({ _id: botId});
+    const account = await collection.findOne({ _id: botId });
     if (!account) {
-      return res.status(404).json({ success: false, message: 'User not found.' });
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found." });
     }
     res.json({ success: true, account: account });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   } finally {
-    if(_client)
-      _client.close();
+    if (_client) _client.close();
   }
 });
 
 // Endpoint to delete an account by bot_id
-app.delete('/deleteUser/:botId', async (req, res) => {
+app.delete("/deleteUser/:botId", async (req, res) => {
   const botId = req.params.botId;
-  if(!botId){
-    return res.status(400).json({success: false, message: "Incorrect botId provided"})
+  if (!botId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Incorrect botId provided" });
   }
 
   let _client = null;
-  try{
-
+  try {
     const client = new MongoClient(mongoURI);
-    _client = client
+    _client = client;
     await client.connect();
 
     const database = client.db("Snipit");
-    const collection = database.collection('users');
+    const collection = database.collection("users");
 
-    await collection.deleteOne({_id: botId})
-    res.json({ success: true, message: `deleted user ${parsedBotId}`});
+    await collection.deleteOne({ _id: botId });
+    res.json({ success: true, message: `deleted user ${parsedBotId}` });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   } finally {
-    _client.close()
+    _client.close();
   }
-})
+});
 
 // Start the server
 app.listen(PORT, () => {
